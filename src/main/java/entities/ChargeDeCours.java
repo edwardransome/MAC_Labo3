c44a7607fab17786140e93f895c4382ddf3dc118
Oceanos1 <evondom@hotmail.com>
2018-01-08 14:15:35 +0100

    done

--- src/main/java/entities/ChargeDeCours.java
+++ src/main/java/entities/ChargeDeCours.java
@@ -1,4 +1,28 @@
 package entities;
 
-public class ChargeDeCours {
+import javax.persistence.Entity;
+
+@Entity
+public class ChargeDeCours implements Enseignant {
+
+    private String nomComplet;
+
+    public ChargeDeCours(){}
+    public ChargeDeCours(String nomComplet){
+        this.nomComplet=nomComplet;
+    }
+
+    public String getNomComplet() {
+        return nomComplet;
+    }
+
+    public void setNomComplet(String nomComplet) {
+        this.nomComplet = nomComplet;
+    }
+
+    @Override
+    public String toString() {
+        return "Charge de cours:" + nomComplet;
+    }
+
 }
--- src/main/java/entities/Professeur.java
+++ src/main/java/entities/Professeur.java
@@ -1,4 +1,51 @@
 package entities;
 
-public class Professeur {
+import javax.persistence.Entity;
+
+@Entity
+public class Professeur implements Enseignant {
+
+    private String prenom;
+    private String nom;
+    private String sigle;
+
+    public Professeur(){}
+    
+    public Professeur(String prenom,String nom) {
+        this.prenom = prenom;
+        this.nom = nom;
+        if (!prenom.isEmpty() && !nom.isEmpty()) {
+            this.sigle = ""+prenom.charAt(0) + nom.charAt(0);
+        }
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getSigle() {
+        return sigle;
+    }
+
+    public void setSigle(String sigle) {
+        this.sigle = sigle;
+    }
+
+    @Override
+    public String toString() {
+        return "Pr: " + nom + " " + prenom + " [" + sigle + "]";
+    }
+
 }
